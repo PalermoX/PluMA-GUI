9f265697a71645102191ee98909925a595b4a8ce
Edward Palermo <eddx540@gmail.com>
2020-09-28 13:53:53 -0400

    adding python files for scraping

--- assets/js/PlugInPool.js
+++ assets/js/PlugInPool.js
@@ -73,4 +73,14 @@ function myFunction() {
     });
   }
 
+  // Accordian button size
+  function myFunction(id) {
+    var x = document.getElementById(id);
+    if (x.className.indexOf("w3-show") == -1) {
+      x.className += " w3-show";
+    } else { 
+      x.className = x.className.replace(" w3-show", "");
+    }
+  }
+
 
